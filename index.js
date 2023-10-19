7a740cead6528c8c2339e61df73c3abd18200d5f
Sahara <sahararod@gmail.com>
2023-10-19 11:39:48 -0600

    se modularizo de nuevo las funciones para implementar la promesa en mdLinks!

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-const mdLinks = require('./mdLinks');
+const readline = require('readline');
+const absolutePath = require('./lib/absolutePath.js');
+const fileExists = require('./lib/fileExists.js');
 
-module.exports = mdLinks;
+function interface(){
+    /* Crear la interfaz READLINE para poder insertar la dirección desde consola con su
+    entrada y salida, se usa RL.QUESTION para hacer la pregunta al usuario */
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    /* Creamos promesa para esperar que el usuario teclee la ruta del archivo */
+    const getPathFile = () => {
+        return new Promise(resolve => {
+            rl.question('Ingresa la ruta del archivo: ', filePath => {
+                resolve(filePath);
+                rl.close();
+            });
+        });
+    }
+    /* Al resolver la promesa lo mandamos al archivo para convertir a ruta absoluta */
+    getPathFile()
+        .then(filePath =>{
+            //return absolutePath(filePath);
+            mdLinks(filePath);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
 
-mdLinks();
+function mdLinks(path){
+  return absolutePath(path)
+   // .then(() => fileExists(path))
+}
+
+interface();
+module.exports = interface;
