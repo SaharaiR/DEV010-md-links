084717642c6586fe86402c1e4bd67c9a687e6b37
Sahara <sahararod@gmail.com>
2023-10-21 22:35:05 -0600

    configure jest, agregue jest/globals, agregue jest-coverage e hice el primer test para la funcion mdLinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,14 +1,26 @@
-/* const mdLinks = require('..');
+const { describe, it, expect } = require('@jest/globals');
+const mdLinks = require('../index');
+
+const expectLinks = [
+  {
+    url: 'https://www.google.com/',
+    text: 'Enlace a Google',
+    file: 'C:\\Users\\Admin\\documents\\github\\dev010-md-links\\ejemplo1.md',
+  },
+  {
+    url: 'https://es.wikipedia.org/',
+    text: 'Enlace a Wikipedia',
+    file: 'C:\\Users\\Admin\\documents\\github\\dev010-md-links\\ejemplo1.md',
+  },
+  {
+    url: 'https://github.com/',
+    text: 'Enlace a GitHub',
+    file: 'C:\\Users\\Admin\\documents\\github\\dev010-md-links\\ejemplo1.md',
+  },
+];
 
 describe('mdLinks', () => {
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
+  it('debería resolver un arreglo con 3 links para un archivo .md con 3 links', () => mdLinks('ejemplo1.md').then((result) => {
+    expect(result).toEqual(expectLinks);
+  }));
 });
-describe('mdLinks', () => {
-  it('debería resolver un arreglo con 3 links para un archivo .md con 3 links', () => {
-    return mdLinks('miArchivo.md').then((result) => {
-      expect...;
-    });
-  });
-}); */
