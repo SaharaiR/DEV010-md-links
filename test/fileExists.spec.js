3baa0e9070174d515437ce35ba05e75babca28e9
Sahara <sahararod@gmail.com>
2023-10-23 14:22:56 -0600

    comence el hito 2, se agrego la funcion statusLinks para agregar al arreglo presentado el codigo de status HTTP

--- test/fileExists.spec.js
+++ test/fileExists.spec.js
@@ -2,7 +2,7 @@ const { describe, it, expect } = require('@jest/globals');
 const fileExists = require('../lib/fileExists');
 
 describe('fileExists', () => {
-  it('debería devolver si el archivo no existe', () => fileExists('ejemplo.md').then((result) => {
+  it('debería devolver false si el archivo no existe', () => fileExists('ejemplo.md').then((result) => {
     expect(result).toEqual(false);
   }));
 });
