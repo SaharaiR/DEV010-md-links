db082349572d9f8256938a30610818b6a97fbe5e
Sahara <sahararod@gmail.com>
2023-10-25 12:03:21 -0600

    agregue un test que faltaba de la funcion statusLink, corregi que debia de mostrar los errores segun el caso (si no existia el archivo, si no era markdown, si no se podia leer, si no tenia links)

--- test/readingFile.spec.js
+++ test/readingFile.spec.js
@@ -4,6 +4,6 @@ const readingFile = require('../lib/readingFile');
 describe('readingFile', () => {
   it('Manda error si no encuentra el archivo', () => readingFile('noExiste.md')
     .catch((error) => {
-      expect(error.code).toEqual('ENOENT');
+      expect(error.message).toEqual('No se puede leer el archivo');
     }));
 });
